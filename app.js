813da6ee9fa44035b6e2f58ac7c1db77def0df31
Hamza <qomaise3@gmail.com>
2022-06-13 07:20:16 +0500

    Call In JS

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-// Understanding Function Context
-function sayHi(){
-   console.log("Hi");
-   console.log(this);
-}
-sayHi();//Hi
-        //window object
-
-let greet={};        
-greet.sayHi=function(){
-            console.log("Hi");
-            console.log(this);
-         }
-greet.sayHi();//Hi
-              //greet
-
-
-
-//Now look Another way of creating functions in JS
+/*The way how we call different objects using a function*/ 
+let person1={name:"Hamza",age:"25"}
+let person2={name:"Qasim",age:"26"}
 function sayHi(){
     console.log("Hi");
-    console.log(this);
- }
-let hey= new sayHi();//Hi
-                    // empty Object
- //This is also known as constructor function
-/* Any time when you call a function with new keyword 
-Javascript create an empty object within the function */ 
+    console.log(this.name);
+}
+sayHi.call(person1);
+sayHi.call(person2);
+// Same thing using function expression
+let person3={name:"Hamza",age:"25"}
+let person4={name:"Qasim",age:"26"}
+let funexp=function (){
+    console.log("Hi");
+    console.log(this.name);
+}
+funexp.call(person3);
+funexp.call(person4);
+// With arguments in call
+let Person={name:"Sajid",age:74}
+let welcome=function(message){
+    console.log(message+", "+this.name);
+}
+welcome.call(Person,"Hi");//First argument must be object
