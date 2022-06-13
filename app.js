9f2d286e375d269efff200bbfd12f0758dab150a
Hamza <qomaise3@gmail.com>
2022-06-13 07:55:44 +0500

    Bind method

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-/*The way how we call different objects using a function*/ 
-let person1={name:"Hamza",age:"25"}
-let person2={name:"Qasim",age:"26"}
-function sayHi(){
-    console.log("Hi");
-    console.log(this.name);
-}
-sayHi.call(person1);
-sayHi.call(person2);
-// Same thing using function expression
-let person3={name:"Hamza",age:"25"}
-let person4={name:"Qasim",age:"26"}
-let funexp=function (){
-    console.log("Hi");
-    console.log(this.name);
-}
-funexp.call(person3);
-funexp.call(person4);
-// With arguments in call
-let Person={name:"Sajid",age:74}
-let welcome=function(message){
-    console.log(message+", "+this.name);
-}
-welcome.call(Person,"Hi");//First argument must be object
+//Apply method
+person1={
+    name:"Hamza",
+    getName:function(){
+        return this.name;
+    }
+};
+person2={name:"sajid"};
+let person1Copy=person1.getName.bind(person2);
+console.log(person1Copy());
